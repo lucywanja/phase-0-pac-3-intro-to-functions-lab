24446a9d6f2f49ce03179f2ff2656089f2c032b5
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-03-18 19:40:33 +0300

    Defined functions that use parameters and parameters with default values

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-// declare the functions
+// Function to return the input string in all uppercase
+
 function shout(string) {
    
-    return string.toUpperCase();
+   return string.toUpperCase();
 
   }
  
   
-
+// Function to return the input string in all lowercase
   function whisper(string) {
     return string.toLowerCase();
 
   }
 
+
+  //Function to log the input string in all caps
   function logShout(string) {
     console.log(string.toUpperCase());
   }
  
+  //Function to log the input string in all lowercase
   function logWhisper(string) {
     console.log(string.toLowerCase());
   }
 
+
+  //  Function to respond based on the input string
   function sayHiToHeadphonedRoommate(string) {
         if (string === string.toLowerCase()) {
             return "I can't hear you!";
